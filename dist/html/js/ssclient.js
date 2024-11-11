3ab52548d76bdc377886ed6f42303542eb7ba167
waku <waku@WakunoMacBook-Air.local>
2024-11-11 19:21:18 +0900

    refactoring

--- dist/html/js/ssclient.js
+++ dist/html/js/ssclient.js
@@ -1,62 +1,59 @@
 // scene vue app
-const $ = (o)=>document.querySelector(o)
+
 export const APP = Vue.createApp({
 	created() {
 	},
 	data() {
 		return {
-			bg_kind:"passthru",
+			bg_kind:"space",
 			bg_mix:1,
 			posx:0,
 			stat_calls:0,
-			stat_poly:0
+			stat_poly:0,
+			base_element:null,
 		}
 	},
 	methods:{
 		// create new app instance
-		async addobj(param) {
-			console.log(param)
-			const prop = {
-				scale:1,
-				basescale:param.param.basescale?param.param.basescale:1, 
-				hcolor:"#ccc"
-			}
-			console.log(prop)
-			const propstr = [':pscale="scale*basescale"']
-			for(let k in param.param) {
-//				if(prop[k]) continue 
-				prop[k] = param.param[k] ;
-				propstr.push(parseFloat(prop[k])!=NaN?`:${k}=${k}`:`:${k}="${k}"`)
-			}
-			console.log(propstr) 
+		async addobj(appdata) {
+			console.log(appdata)
 			const app2 = Vue.createApp({
 				data() {
-					prop.key = 0
-					prop.comppath = param.path
-					prop.ccompo = null
-					return prop
+					return {
+						cscale:1,
+						hcolor:"#ccc",
+						param:{'basescale':1},
+						key:0,
+						comppath:	 appdata.path,
+						ccompo: null 		
+					}
 				},
 				created() {
 					this.loadComponent(); // コンポーネントの初期ロード
 				},
 				methods:{
+					setparam(param) {
+						for(let k in param) {
+							const v = param[k] 
+							this.param[k] = isNaN(parseFloat(v))?v:parseFloat(v) ;
+							console.log(`set ${k} to ${param[k]}`)
+						}						
+					},
 					async loadComponent() {
-						// 現在時刻をクエリパラメータとして付加し、キャッシュを防止
 						const timestamp = new Date().getTime();
-					
 						// 動的にコンポーネントをインポートし、キャッシュを避ける
 						const module = await import(`${this.comppath}?${timestamp}`);
-						console.log(module.default)
-						this.ccompo = module.default;
+						this.ccompo = Vue.shallowRef(module.default); //warning避け
+						console.log(this.ccompo) 
 					},
 					async reloadComponent() {
 						await this.loadComponent();
 						this.componentKey += 1; // キーを更新して再描画をトリガー
-					}					
+					}		
 				},
 				template: `<a-box width=0.02 height=0.02 depth=0.02  :color="hcolor" opacity=0.8  grabbable2="distance:.04" ss_grabbase>
 					<a-entity position="0 0.01 0" >
-						<component :key=key :is=ccompo ${propstr.join(" ")} />
+						<component :key=key :is=ccompo :pscale="cscale*param.basescale" v-bind="param" />
 					</a-entity>
 				</a-box>`
 			})
@@ -68,19 +65,25 @@ export const APP = Vue.createApp({
 			if(this.posx > 0.3) this.posx -= 0.58
 
 			const appinst = app2.mount(el)
-			$('#base').appendChild(el) 
-			return {id:param.id,app:appinst,el:el,time:new Date().getTime()}
-		}
+			appinst.setparam(appdata.param) 
+			this.base_element.appendChild(el) 
+			return {id:appdata.id,app:appinst,el:el,time:new Date().getTime()}
+		},
+		registerComponent(name,data) {
+			if(AFRAME.components[name]) delete AFRAME.components[name]
+			AFRAME.registerComponent(name,data) 
+		}		
 	}
 })
 
+const $ = (o)=>document.querySelector(o)
 // A-Frame components 
 AFRAME.registerComponent('stat', {
 	tick:function() {
-		if(!APPINST) return 
+		if(!SPSHELL) return 
 		let info = this.el.sceneEl.renderer.info.render
-		APPINST.stat_calls = info.calls 
-		APPINST.stat_poly = info.triangles 
+		SPSHELL.stat_calls = info.calls 
+		SPSHELL.stat_poly = info.triangles 
 	}	
 })
 AFRAME.registerComponent('ss_grabbase', {
