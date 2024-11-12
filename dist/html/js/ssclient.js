4434846626c0f47fb11f3e130643816df7d808fd
waku <waku@WakunoMacBook-Air.local>
2024-11-13 01:16:52 +0900

    add pid to instance

--- dist/html/js/ssclient.js
+++ dist/html/js/ssclient.js
@@ -25,7 +25,8 @@ export const APP = Vue.createApp({
 						param:{'basescale':1},
 						key:0,
 						comppath:	 appdata.path,
-						ccompo: null 		
+						ccompo: null,
+						pid: appdata.pid 		
 					}
 				},
 				created() {
@@ -51,12 +52,18 @@ export const APP = Vue.createApp({
 						this.componentKey += 1; // キーを更新して再描画をトリガー
 					}		
 				},
-				template: `<a-box width=0.02 height=0.02 depth=0.02  :color="hcolor" opacity=0.8  grabbable2="distance:.04" ss_grabbase>
+				mounted() {
+					this.$refs.gbox.setAttribute("ss_grabbase","pid:"+this.pid)
+//					console.log(this.$refs.gbox.innerHTML) 
+				},
+				template: `<a-box ref="gbox" width=0.02 height=0.02 depth=0.02  :color="hcolor" opacity=0.8  grabbable2="distance:.06" ss_grabbase>
 					<a-entity position="0 0.01 0" >
 						<component :key=key :is=ccompo :pscale="cscale*param.basescale" v-bind="param" />
 					</a-entity>
 				</a-box>`
 			})
+			
+			//初期位置決め
 			const el = document.createElement("a-entity")
 			let posy = document.querySelector("[camera]").object3D.position.y	
 			if(posy==0) posy=1
@@ -64,11 +71,12 @@ export const APP = Vue.createApp({
 			this.posx += 0.1 
 			if(this.posx > 0.3) this.posx -= 0.58
 
-			const appinst = app2.mount(el)
-			appinst.setparam(appdata.param) 
-			this.base_element.appendChild(el) 
+			const appinst = app2.mount(el)	//アプリマウント
+			appinst.setparam(appdata.param) //初期パラメータセット
+			this.base_element.appendChild(el) // DOMにappend
 			return {id:appdata.id,app:appinst,el:el,time:new Date().getTime()}
 		},
+		// サービス関数 AFRAME.registerComponentのラッパ
 		registerComponent(name,data) {
 			if(AFRAME.components[name]) delete AFRAME.components[name]
 			AFRAME.registerComponent(name,data) 
@@ -87,9 +95,12 @@ AFRAME.registerComponent('stat', {
 	}	
 })
 AFRAME.registerComponent('ss_grabbase', {
+	schema:{
+		pid:{type:"number",default:0}
+	},
 	init:function() {
 		this.el.addEventListener("grab",ev=>{
-			console.log("grabbed!")
+			console.log("grabbed! "+this.data.pid)
 			console.log(ev.detail) 
 		})
 	},
