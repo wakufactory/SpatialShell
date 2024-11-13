974610b5374637f27f1db30c627ce9b20f6aca0c
waku <wakuoe@dhcp113.kud.lang>
2024-11-13 14:55:26 +0900

    add sample app

--- dist/html/apps/basic.js
+++ dist/html/apps/basic.js
@@ -13,7 +13,8 @@ export default {
 		speed:{
 			type:Number,
 			default:60
-		}
+		},
+		axis:String
 	},  
 	methods:{
 		scale() {
@@ -22,24 +23,24 @@ export default {
 	},
 	template: 	// A-Frame template 
 	'<a-entity :scale="scale()"  position="0 .06 0" > \
-		<a-box visible=true scale=".1 .1 .1" position="0 0.05 0"  :color="color" :ss-rot="`speed:${speed}`"></a-box> \
+		<a-box visible=true scale=".1 .1 .1" position="0 0.05 0"  :color="color" :ss-move="`speed:${speed};axis:${axis}`"></a-box> \
 	</a-entity>'
 }
 
 // A-Frame component (user wrapper function)
-SPSHELL.registerComponent('ss-rot',{
+SPSHELL.registerComponent('ss-move',{
 	schema: {
 		speed:{type:"number",default:60},
 		axis:{default:"Y"}
 	},
 	init:function() {
-		this.rot = {x:0,y:0,z:0}
+		this.move = {x:0,y:0,z:0}
 	},
 	tick:function(time, timeDelta) {
-		if(this.data.axis=="X") this.rot.x = THREE.MathUtils.degToRad(time/this.data.speed)
-		if(this.data.axis=="Y") this.rot.y = THREE.MathUtils.degToRad(time/this.data.speed)
-		if(this.data.axis=="Z") this.rot.z = THREE.MathUtils.degToRad(time/this.data.speed)
-		this.el.object3D.rotation.set(this.rot.x,this.rot.y,this.rot.z)
+		if(this.data.axis=="X") this.move.x = (Math.sin(time/this.data.speed)+1)*0.01
+		if(this.data.axis=="Y") this.move.y = (Math.sin(time/this.data.speed)+1)*0.01
+		if(this.data.axis=="Z") this.move.z = (Math.sin(time/this.data.speed)+1)*0.01
+		this.el.object3D.position.set(this.move.x,this.move.y,this.move.z)
 	}
 })
 
---
+++ dist/html/apps/nocode.js
@@ -0,0 +1,35 @@
+/*
+	SPATIAL SHELL APP
+	basic nocode boilerplate
+*/  
+// Vue component definition
+export default {  
+	props:{  
+		pscale:Number,
+		speed:{
+			type:Number,
+			default:2000
+		}
+	}, 
+	template: // A-Frame template
+	'<a-entity>\
+	<a-mixin id="torus1" radius-tubular="0.1"></a-mixin> \
+	<a-mixin id="torus2" segments-tubular="60"></a-mixin> \
+	<a-entity :scale="`${pscale} ${pscale} ${pscale}`" position="0 .06 0" > \
+	<a-icosahedron scale=".1 .1 .1" position="0 0.1 0" color="rgba(234,194,99,1)" \
+		:animation="`property: rotation; easing:linear;to: 0 0 360 ; loop: true; dur:${speed};`" \
+		shadow material="metalness:0.5;roughness:0.5"> \
+		<a-torus arc="360" radius="1.1" mixin="torus1 torus2" position="0 0 0"  color="rgba(194,23,116,1)" \
+			:animation="`property:rotation; easing:linear; to: 0 360 0 ; loop: true; dur: ${speed*1.1};`" shadow> \
+			<a-torus arc="360" radius="1.5" mixin="torus1 torus2" position="0 0 0"  color="rgba(146,194,23,1)" \
+				:animation="`property:rotation; easing:linear; to: 360 0 0; loop: true; dur: ${speed*1.2};`" shadow> \
+				<a-torus arc="360" radius="1.9" mixin="torus1 torus2"  position="0 0 0"  color="#2dbadd" \
+					:animation="`property: rotation; easing:linear; to: 0 360 0 ; loop: true; dur: ${speed*1.3};`" shadow> \
+				</a-torus> \
+			</a-torus> \
+		</a-torus> \
+	</a-icosahedron> \
+	</a-entity></a-entity>'
+}
+
+
